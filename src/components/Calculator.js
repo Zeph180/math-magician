c7958c3221e038218d1e078a0e948bb3da890990
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-02-21 14:42:31 +0300

    Add logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,18 +1,55 @@
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const obj = {
+  total: null,
+  next: null,
+  operation: null,
+};
+
 export default function Calculator() {
-  const nums = ['Ac', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+  const [output, setOutput] = useState(obj);
+
+  const nums = ['AC', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+  const handleBtnClick = (e) => {
+    const btn = e.target.name;
+    return setOutput(calculate(output, btn));
+  };
+
+  const screen = (item) => {
+    if ((!item.total) && (!item.next)) {
+      return '0';
+    }
+    if (!item.total) {
+      return item.next;
+    }
+    if (item.next && item.total && item.operation) {
+      return item.next;
+    }
+    return item.total;
+  };
 
   return (
     <section id="calc">
       <input
         type="text"
-        value="0"
+        value={screen(output)}
         disabled
         id="out"
       />
       <section id="input-sec">
         <article id="numbers">
           {
-            nums.map((num) => <button key={num} type="button">{num}</button>)
+            nums.map((num) => (
+              <button
+                key={num}
+                type="button"
+                name={num}
+                onClick={handleBtnClick}
+              >
+                {num}
+              </button>
+            ))
           }
         </article>
       </section>
